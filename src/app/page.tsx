84061aa72e839972768005318ec08af405537741
madarasz <mr.madarasz.istvan@gmail.com>
2025-09-07 15:50:54 +0200

    fix link issue on page you never see

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 export default function RootPage() {
   return (
     <div>
       <h1>Welcome</h1>
       <p>You should not see this page in production, this is dev only. Production server should redirect you to the appropriate locale.</p>
       <ul>
-        <li><a href="/en">English</a></li>
-        <li><a href="/hu">Magyar</a></li>
+        <li><Link href="/en">English</Link></li>
+        <li><Link href="/hu">Magyar</Link></li>
       </ul>
     </div>
   )
