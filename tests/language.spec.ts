add70842d4cd0dc21e0f27f3955acb4090074231
madarasz <mr.madarasz.istvan@gmail.com>
2026-06-28 12:48:44 +0200

    test update

--- tests/language.spec.ts
+++ tests/language.spec.ts
@@ -11,8 +11,6 @@ test.describe('Language Tests', () => {
     await expect(page.locator('text=Rólunk').first()).toBeVisible();
     await expect(page.locator('text=Üdvözlünk').first()).toBeVisible();
     await expect(page.locator('text=Közösségünk').first()).toBeVisible();
-    await expect(page.locator('text=Akciódús').first()).toBeVisible();
-    await expect(page.locator('text=Gyors').first()).toBeVisible();
   });
 
   test('English page displays correct texts', async ({ page }) => {
@@ -24,8 +22,6 @@ test.describe('Language Tests', () => {
     await expect(page.locator('text=Calendar').first()).toBeVisible();
     await expect(page.locator('text=About us').first()).toBeVisible();
     await expect(page.locator('text=Welcome').first()).toBeVisible();
-    await expect(page.locator('text=group').first()).toBeVisible();
-    await expect(page.locator('text=Action-packed').first()).toBeVisible();
-    await expect(page.locator('text=fast').first()).toBeVisible();
+    await expect(page.locator('text=community').first()).toBeVisible();
   });
 });
